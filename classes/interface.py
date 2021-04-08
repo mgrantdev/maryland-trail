b2b41d6e2b28e09ec4a4f5a7d415755058911ab9
Matthew Grant <mgrant.xi@gmail.com>
2021-04-08 12:44:28 -0400

    Updated interface naming convention

--- classes/interface.py
+++ classes/interface.py
@@ -9,7 +9,7 @@ class Interface(object):
     window_width =  None
     window_height = None
     dynamic_frame = None
-    pages = {}
+    scenes = {}
 
     def __init__(self, loader):
         # initalize window and loader
@@ -28,27 +28,29 @@ class Interface(object):
         # toolbar image
         current_dir = os.path.abspath(os.path.dirname(__file__))
         filepath = os.path.join(current_dir, "../bin/images/welcome_maryland.png")
-        maryland_welcome_sign = Image.open(filepath)
-        maryland_welcome_sign = maryland_welcome_sign.resize((round(window_width * 0.1), round(window_height * 0.2)), Image.ANTIALIAS)
-        maryland_welcome_sign_tk = ImageTk.PhotoImage(maryland_welcome_sign)
-        image_label = tkinter.Label(image=maryland_welcome_sign_tk, borderwidth=0)
-        image_label.image = maryland_welcome_sign_tk
-        image_label.place(relx=0.05, rely=0.05)
-        title_label = tkinter.Label(bg="#000", text="The Maryland Trail", fg="#fff", font=("Arial", 20))
-        self.window.update()
-        title_label.place(x=(toolbar_frame.winfo_width() - image_label.winfo_width())/4, y=round(window_height * 0.2) + round(window_height * 0.05) + 5)
+        toolbar_buttons['image'] = {}
+        toolbar_buttons['image']['welcome_sign'] = Image.open(filepath)
+        toolbar_buttons['image']['welcome_sign'] = toolbar_buttons['image']['welcome_sign'].resize((round(window_width * 0.1), round(window_height * 0.2)), Image.ANTIALIAS)
+        toolbar_buttons['image']['welcome_sign_widget'] = ImageTk.PhotoImage(toolbar_buttons['image']['welcome_sign'])
+        toolbar_buttons['image']['label'] = tkinter.Label(image=toolbar_buttons['image']['welcome_sign_widget'], borderwidth=0)
+        toolbar_buttons['image']['label'].image = toolbar_buttons['image']['welcome_sign_widget']
+        toolbar_buttons['image']['label'].place(relx=0.05, rely=0.05)
+        toolbar_buttons['image']['heading'] = tkinter.Label(bg="#000", text="The Maryland Trail", fg="#fff", font=("Arial", 20))
+        self.window.update() # updates window dimensions
+        toolbar_buttons['image']['heading'].place(x=(toolbar_frame.winfo_width() - toolbar_buttons['image']['label'].winfo_width())/4, y=round(window_height * 0.2) + round(window_height * 0.05) + 5)
         # create dynamic frame
         dynamic_frame = tkinter.Frame(self.window, bg="#1a1a1a")
         dynamic_frame.place(relx=0.2, rely=0, relwidth=0.8, relheight=1)
         # create welcome frame
-        self.window.update()
-        self.pages['welcome'] = tkinter.Frame(dynamic_frame, bg="#1a1a1a")
-        self.pages['welcome'].place(relx=0.1, rely=0.1, relwidth=0.8, relheight=.9)
-        welcome_text = "Welcome to the Maryland Trail!\n\nOn your journey you will have the opportunity to explore the Old Line State\n in all of its grandeur, with YOU deciding what to explore and how to explore it.\n\nMake wise decisions and make the most of your trip.\n\nSafe travels!\n"
-        welcome_text_1 = tkinter.Label(self.pages['welcome'], bg="#1a1a1a", text=welcome_text, fg="#fff", font=("Arial", 20))
-        welcome_text_1.pack()
-        continue_label = tkinter.Label(self.pages['welcome'], bg="#1a1a1a", text="press <SPACE> to continue", fg="#fff", font=("Arial", 20))
-        continue_label.place(relx=.15, rely=.9, relwidth=.7)
+        self.window.update() # updates window dimensions
+        self.scenes['scene1'] = {}
+        self.scenes['scene1']['frame'] = tkinter.Frame(dynamic_frame, bg="#1a1a1a")
+        self.scenes['scene1']['frame'].place(relx=0.1, rely=0.1, relwidth=0.8, relheight=.9)
+        self.scenes['scene1']['text'] = "Welcome to the Maryland Trail!\n\nOn your journey you will have the opportunity to explore the Old Line State\n in all of its grandeur, with YOU deciding what to explore and how to explore it.\n\nMake wise decisions and make the most of your trip.\n\nSafe travels!\n"
+        self.scenes['scene1']['text_label'] = tkinter.Label(self.scenes['scene1']['frame'], bg="#1a1a1a", text=self.scenes['scene1']['text'], fg="#fff", font=("Arial", 20))
+        self.scenes['scene1']['text_label'].pack()
+        self.scenes['scene1']['continue_label'] = tkinter.Label(self.scenes['scene1']['frame'], bg="#1a1a1a", text="press <SPACE> to continue", fg="#fff", font=("Arial", 20))
+        self.scenes['scene1']['continue_label'].place(relx=.15, rely=.9, relwidth=.7)
         # finish window creation
         self.window.mainloop()
 
